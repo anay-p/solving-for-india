698adadb01648cfc56cc6ed9f8c7647bcd72c835
Anay Pareek <anaypareek2003@gmail.com>
2023-05-23 03:28:45 +0530

    Clean up firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: "solving-for-india-381319",
-  storageBucket: "solving-for-india-381319.appspot.com",
-  messagingSenderId: "819073826445",
-  appId: "1:819073826445:web:52c61545cc6415146f6cc4"
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const createUserDocument = async (userx) => {
-  if (!userx) return;
-  const userRef=collection(db, 'users');
-  // const snapshot=await userRef.get();
-  // if(!snapshot.exists){
-  //   const {email}=userx.Email;
-  //   const {displayName}=userx.Name;
-  // }
-  addDoc(userRef,userx)
-  .then(docRef => {
-    console.log("Document has been added successfully");
+export const createUserDocument = async (userData) => {
+  const collectionRef = collection(db, 'users');
+  addDoc(collectionRef, userData)
+  .then(() => {
+    console.log("User data saved successfully");
   })
   .catch(error => {
     console.log(error);
